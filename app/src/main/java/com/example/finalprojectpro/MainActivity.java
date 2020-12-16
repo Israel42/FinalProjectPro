aa2e23d6ee133c7c9a81bda877579cf56b0221e8
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-12-16 16:41:57 +0300

    permission check

--- app/src/main/java/com/example/finalprojectpro/MainActivity.java
+++ app/src/main/java/com/example/finalprojectpro/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.finalprojectpro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
@@ -15,12 +21,13 @@ import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends BaseActivity {
   MeowBottomNavigation bottomNavigation;
-
+  private final static int PERMISSION=99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkphonepermission();
         bottomNavigation=findViewById(R.id.bottom_nav);
         bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_hotel_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_book_24));
@@ -53,5 +60,24 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+    }
+
+    private void checkphonepermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+            } else {
+                checkphonepermission();
+            }
+        }
     }
 }
